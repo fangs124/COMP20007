033a85cd2c8dbc1f3b93c6a078770bdb927debfd
fangs124 <adrianhendrawan24@yahoo.com>
2016-04-01 12:59:06 +1100

    Finished up to part 4.
    Implemented kahn's sorting algorithm and the verification function.
    
    To do: rewrite everything in a nicer and commented format...

--- Ass1/toposort.c
+++ Ass1/toposort.c
@@ -54,15 +54,77 @@ List dfs_sort(Graph graph) {
     return sorted;
 }
 
+bool is_equal(void *aim, void *node){
+	return(bool)((int)aim == (int)node);
+}
+
+void kahn_remove_edge(Graph graph, int v_index, int n_index, int *edge_count){
+	del(is_equal,v_index, &graph->vertices[n_index].in);
+	*edge_count = *edge_count-1;
+	fprintf(stderr, "in remove: size = %d\n", *edge_count);
+	return;
+}
 /* Returns a list of topologically sorted vertices using the Kahn method */
 List kahn_sort(Graph graph) {
+	fprintf(stderr, "initial: size = %d\n", graph->size);
+	int edge_count = graph->size;
 	int order = graph->order;
 	List sorted = NULL;
+	List source = NULL;
+	int i;
+	/* add all vertices with no incoming edge */
+	for(i = 0; i < order; i++){
+		if(graph->vertices[i].in == NULL){
+			source = push(source, i);
+		}
+	}
+	List ptr;
+	int v_index;//srt
+	int n_index;//dst
+	while(source != NULL){
+		v_index = (int)pop(&source);
+		insert(&graph->vertices[v_index], &sorted);
+		ptr = graph->vertices[v_index].out;
+		while(ptr != NULL){
+			n_index = (int)ptr->data;
+			kahn_remove_edge(graph, v_index, n_index, &edge_count);
+			fprintf(stderr, "in sort: size = %d\n", edge_count);
+			if(graph->vertices[n_index].in == NULL){
+				source = push(source, n_index);
+			}
+			ptr = ptr->next;
+		}
+	}
 	
-    return NULL;
+	if(edge_count != 0){
+		fprintf(stderr, "E: graph is cyclical\n");
+		exit(EXIT_FAILURE);
+	}
+
+    return sorted;
 }
 
 /* Uses graph to verify vertices are topologically sorted */
 bool verify(Graph graph, List vertices) {
-    return false;
+	bool processed[graph->order];
+	int i;
+	for(i = 0; i < graph->order; i++){
+		processed[i] = false;
+	}
+	int v;
+	List v_ptr = vertices;
+	List out_ptr;
+	while(v_ptr->next != NULL){
+		v = ((Vertex)v_ptr->data)->id;
+		processed[v] = true;
+		out_ptr = ((Vertex)v_ptr->data)->out;
+		while(out_ptr != NULL){
+			if(processed[(int)out_ptr->data] == true){
+				return false;
+			}
+			out_ptr = out_ptr->next;
+		}
+		v_ptr = v_ptr->next;
+	}
+    return true;
 }
