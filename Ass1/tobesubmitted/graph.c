26289a50453d7eaff4d0f358b8f9b909d21566ff
fangs124 <adrianhendrawan24@yahoo.com>
2016-04-02 16:34:05 +1100

    Implemented (partially) graphio.c and graph.c
    Implemented most of graphio.c , need to implement print_graph() next. Relevant function on graph.c was also implemented.
    removed temp (irrelevant file)

--- Ass1/tobesubmitted/graph.c
+++ Ass1/tobesubmitted/graph.c
@@ -8,7 +8,14 @@
 
 /* Returns a pointer to a new graph with order vertices */
 Graph new_graph(int order) {
-    return NULL;
+	Graph graph;
+	graph->order = order;
+	graph->size = 0;
+
+	/* assign memory for vertices */
+	graph->vertices = (Vertex) malloc(sizeof(*graph->vertices) * order);
+	assert(graph->vertices != NULL);
+    return graph;
 }
 
 /* Returns whether aim and vertex are pointing to the same location */
@@ -23,7 +30,10 @@ bool id_eq(void *aim, void *vertex) {
 
 /* Add the edge from v1 to v2 to graph */
 void add_edge(Graph graph, int v1, int v2) {
-
+	graph->size++;
+	graph->vertices[v1].out = push(graph->vertices[v1].out, v2);
+	graph->vertices[v2].in  = push(graph->vertices[v2].in , v1);
+	return;
 }
 
 /* Free the memory allocated to graph */
--- Ass1/tobesubmitted/graphio.c
+++ Ass1/tobesubmitted/graphio.c
@@ -16,9 +16,126 @@
 
 #define MAX_LINE_LEN 256
 
+#define MAX_DIGIT 4 //max digit for vertex id
+
+typedef enum { //created
+    VCOUNT    = 0,
+    VLABEL    = 1,
+    DEDGES    = 2,
+} input_stage;
+
+//----// sub-functions 
+
+/* Assigns relevant data to pointed vertex */
+void assign_vertex(Vertex vertex, int id, char *buffer) { //created
+    /* assigning data */
+    vertex->id = id;
+    vertex->in = NULL;
+    vertex->out = NULL;
+    
+    /* length of label */
+    size_t label_len = strlen(buffer) - 1; //compensating for '\n' and '\0'
+
+    /* assign memory for label */
+    vertex->label = malloc(label_len);
+    assert(vertex->label != NULL);
+
+    vertex->label = memcpy(vertex->label, buffer, label_len);
+    vertex->label[label_len] = '\0'; //replacing '\n' with '\0'
+    return;
+}
+
+/* Assigns value pair form buffer to the edge: [v1, v2] */
+void get_value_pair(int *edge, char *buffer) { //created
+    /* assign memory for integer */
+    char* integer = (char*) malloc(sizeof(char) * MAX_DIGIT);
+    assert(integer != NULL);
+
+    int start = 0; //starting index of v1
+    int digit_count = 0;
+    char *ptr;
+
+    for(ptr = buffer; ptr != '\0'; ptr++){
+        /* count digit */
+        if(*ptr >= '0' && *ptr <= '9'){ //vertex id is numeric only
+            digit_count++;
+        }
+
+        /* get v1 */
+        else if(*ptr == ' '){
+            integer = memcpy(integer, &buffer[start], digit_count);
+            /* assign v1 to edge: [v1, v2] */
+            edge[0] = atoi(integer);
+            
+            //starting index of v2
+            start = digit_count + 1;
+            digit_count = 0;
+        }
+
+        /* get v2 */
+        else if(*ptr == '\n'){
+            integer = memcpy(integer, &buffer[start], digit_count);
+            /* assign v2 to edge: [v1, v2] */
+            edge[1] = atoi(integer);
+        }
+    }
+
+    /* free allocated memory */
+    free(integer);
+    integer = NULL;
+    return;
+}
+
+//----//
+
 /* Loads the graph from input */
-Graph load_graph(char *input) {
-    return NULL;
+Graph load_graph(char *input) { //impelemented
+    /* open file */
+    FILE *file = fopen(input, "r");
+
+    /* assign memory for input buffer */
+    char *buffer = (char*) malloc(sizeof(char) * MAX_LINE_LEN);
+    assert(buffer != NULL);
+
+    int n; //vertex index
+    int edge[2]; //ordered pair of vertices: [v1, v2]
+
+    Graph graph;
+    input_stage stage = VCOUNT;
+
+    /* get per-line string from input */
+    while((buffer = fgets(buffer, MAX_LINE_LEN, file)) != NULL){
+        /* count and allocate memory for vertices */
+        if(stage == VCOUNT){
+            graph = new_graph(atoi(buffer));
+            stage++;
+        }
+
+        /* assign vertex labels */
+        else if(stage == VLABEL){
+            //process nth vertex
+            assign_vertex(&graph->vertices[n], n, buffer);
+            
+            n++; 
+            if(n == graph->order){
+                //move to next stage after all vertices
+                stage++;
+            }
+        }
+
+        /* assign directed edges */
+        else if(stage == DEDGES){
+            //get [v1, v2] from buffer
+            get_value_pair(&edge[0], buffer);
+            //create edge [v1, v2]
+            add_edge(graph, edge[0], edge[1]);
+        }
+    }
+
+    /* free allocated memory */
+    free(buffer);
+    buffer = NULL;
+    return graph;
 }
 
 /* Prints the graph */
@@ -26,7 +143,9 @@ void print_graph(char *output, Graph graph) {
 }
 
 /* Prints the destination vertex label surrounded by spaces */
-void print_vertex_label(FILE *file, void *vertex) {
+void print_vertex_label(FILE *file, void *vertex) { //implemented
+    if (vertex)
+        fprintf(file, " %s ", ((Vertex)vertex)->label);
 }
 
 /* Prints the id of a vertex then a newline */
